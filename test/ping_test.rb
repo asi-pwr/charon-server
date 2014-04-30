3a8c3d514b7d20ede8620d29a1e6dbef7ee316a6
Szymon Szydełko <szymon@szydelko.pw>
2014-04-30 14:23:20 +0200

    Add more pinging tests

--- test/ping_test.rb
+++ test/ping_test.rb
@@ -11,9 +11,28 @@ class PingTest < Minitest::Test
     Sinatra::Application
   end
 
+  id = "123456"
+
   def test_it_acks_ping
-    get '/ping'
+    get "/ping/?id=#{id}"
     assert_equal '{"status":"true"}', last_response.body
+  end
+
+  def test_missing_dev_id
+    get '/ping'
+    # ma być błąd brakującego parametru
+    assert_equal '{"status":"false"}', last_response.body
+  end
+
+  def test_bad_dev_id
+    get "/ping/?id=#{id}4"
+    #ma być zły format
+    assert_equal '{"status":"false"}', last_response.body
+  end
+
+  def test_auth_err
+
+    # dunno jak to zrobić
   end
 
 end
